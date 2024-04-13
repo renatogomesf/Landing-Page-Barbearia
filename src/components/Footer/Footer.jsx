7908b3d1180db14e5bd06f16c2974a284afe2751
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-04-13 11:14:18 -0300

    correção do scrollreveal

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-// import ScrollReveal from "scrollreveal";
+import ScrollReveal from "scrollreveal";
 import { useEffect } from "react";
 
 import { Content, Div, Img, Wrapper, Whats } from "./style";
