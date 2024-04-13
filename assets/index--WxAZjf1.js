907ae96a9d5e266b79972296653ea907970397f8
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-04-13 11:18:15 -0300

    Updates

--- assets/index--WxAZjf1.js
+++ assets/index--WxAZjf1.js
@@ -505,8 +505,6 @@ Error generating stack: `+o.message+`
         height: 100vh;
 
         background-color: ${e=>e.theme.brand_color.cor01};
-
-        transition: 0.1s;
     }
 
 `,Am={brand_color:{cor01:"#06121E",cor02:"#040f1a",cor03:"#0c2135",cor04:"#090d11",cor05:"#06090c",cor06:"#1a1f25"},neutral_color:{cor01:"#232224",cor02:"#161617",cor03:"#353436",cor04:"#ffffff",cor05:"#fefbff"},auxiliary_color:{cor01:"#D94140",cor02:"#b42b2b",cor03:"#d34040"}};wl.createRoot(document.getElementById("root")).render(C.jsx(ne.StrictMode,{children:C.jsxs(G1,{theme:Am,children:[C.jsx(Fm,{}),C.jsx(Mm,{})]})}));
